6a1afccd7452f000ab34f77be44aed823f95ce48
Riman <steamid301@gmail.com>
2025-11-09 02:07:33 +0100

    Added invalid route handler

--- index.js
+++ index.js
@@ -14,9 +14,22 @@ app.use(express.json());
 app.use("/api/foods", foodsRouter);
 app.use("/api/requests", requestsRouter);
 
+
+
 // Base route
 app.get("/", (req, res) => {
   res.send("PlateShare backend is running ✅");
+});
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  console.error("Server Error:", err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
 });
 
 // Connect to DB and start server
